9860528bc7af3852d7d667375bb7f92895d993a8
Irmina Stepien <irm.stepien@gmail.com>
2019-05-24 09:46:08 +0200

    Play method simplified

--- app/src/main/java/com/example/birdsvoices/MainActivity.java
+++ app/src/main/java/com/example/birdsvoices/MainActivity.java
@@ -6,8 +6,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.GridLayout;
-import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,49 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void play(View view) {
 
-        int bird = Integer.parseInt((String) view.getTag());
-        Log.i("info","bird tag: "+bird );
-
-        switch (bird){
-                case 0:
-                mediaPlayer = MediaPlayer.create(this, R.raw.bazant);
-                mediaPlayer.start();
-                break;
-            case 1:
-                mediaPlayer = MediaPlayer.create(this, R.raw.dzwoniec);
-                mediaPlayer.start();
-                break;
-            case 2:
-                mediaPlayer = MediaPlayer.create(this, R.raw.gil);
-                mediaPlayer.start();
-                break;
-            case 3:
-                mediaPlayer = MediaPlayer.create(this, R.raw.kos);
-                mediaPlayer.start();
-                break;
-            case 4:
-                mediaPlayer = MediaPlayer.create(this, R.raw.kwiczol);
-                mediaPlayer.start();
-                break;
-            case 5:
-                mediaPlayer = MediaPlayer.create(this, R.raw.puszczyk);
-                mediaPlayer.start();
-                break;
-            case 6:
-                mediaPlayer = MediaPlayer.create(this, R.raw.slowik_szary);
-                mediaPlayer.start();
-                break;
-            case 7:
-                mediaPlayer = MediaPlayer.create(this, R.raw.wrobel);
-                mediaPlayer.start();
-                break;
+        if (mediaPlayer!= null) {
+            mediaPlayer.release();
+            mediaPlayer= null;
         }
+        String birdTag = view.getTag().toString();
+        Log.i("info", "bird tag: " + birdTag);
+        mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(birdTag, "raw", getPackageName()));
+        mediaPlayer.start();
 
 
+    }
+
+    public void stop(View view) {
+        mediaPlayer.stop();
+        Log.i("info", "stoped");
     }
 }
